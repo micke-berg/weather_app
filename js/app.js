403191dcbd11708047e384eeb711f415669065e8
Micke Berg <micke.berg.dev@gmail.com>
2019-12-08 15:18:50 +0100

    Changed interval on setInterval() for user position. From 50000 to 30000. Removed the get fahrenheit option

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ function setPosition(position) {
   setInterval(() => {
     console.log(latitude);
     getWeather(latitude, longitude);
-  }, 60000);
+  }, 30000);
 }
 
 // Show error if problem with getting geolocation
@@ -85,30 +85,6 @@ function displayWeather() {
   locationElement.innerHTML = `${weather.city} ${weather.country}`;
   console.log(weather.country);
 }
-
-// Convert Celsius to Fahrenheit
-// function celsiusToFahrenheit(temperature) {
-//   return (temperature * 9) / 5 + 32;
-// }
-
-// User click changes from Celsius to Fahrenheit or vice versa
-// tempElement.addEventListener('click', () => {
-//   if (weather.temperature.value === undefined) return;
-
-//   if (weather.temperature.unit === 'celsius') {
-//     let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-
-//     fahrenheit = Math.floor(fahrenheit);
-
-//     tempElement.innerHTML = `${fahrenheit}˚ <span>F</span>`;
-
-//     weather.temperature.unit = 'fahrenheit';
-//   } else {
-//     tempElement.innerHTML = `${weather.temperature.value}˚ <span>C</span>`;
-
-//     weather.temperature.unit = 'celsius';
-//   }
-// });
 
 //Add Zeros to day and date if needed
 function addZero(n) {
